ece42f6ab4fe9c2ac923b3f4159c8a0a817ef8ce
Daniele Cattaneo <daniele.cattaneo@alice.it>
2017-03-25 13:43:19 +0100

    Minor optimizations to Ukkonen.
    Got rid of McCreight’s terminology and now avoids re-searching the prefix back from the beginning of the arc every time.

--- ukkonen.c
+++ ukkonen.c
@@ -11,55 +11,56 @@ treenode_t *ukkonen(const char *str)
   treenode_t *root = calloc(1, sizeof(treenode_t));
   root->suffix_link = root;
   
-  treenode_t *active_node = root;
+  /* loop invariant: active_pos must be the position in the tree where the
+   * preexisting suffix we want to extend ends */
+  splitpoint_t active_pos = {root, NULL, 0};
   
   int j=0;
   /* i: character read at this stage + 1 */
   for (int i=1; i<=s.end; i++) {
     range_t cs = {0, i};
     
-    /* j: starting character of the suffix we are appending str[i] to */
+    /* j: starting character of the suffix we are appending str[i-1] to */
     for (; j<i; j++) {
       range_t csfx = {j, cs.end};
-      
-      splitpoint_t headsp;
     
-      /* adjust active_node to be exactly at the head */
-      range_t partial_head = active_node->node_val;
-      int rest_end = RANGE_LEN(partial_head) + j;
-      range_t rest = {rest_end, cs.end};
-      headsp = slowscan(str, &rest, active_node);
-            
       /* rule 3: is {j, i} in the tree? */
-      if (headsp.arcpos + RANGE_LEN(headsp.parent->node_val) == RANGE_LEN(csfx)) {
-        /* yes: fast forward rule 3 for all other j's and increment i */
+      range_t rest = {i-1, i};
+      active_pos = slowscan(str, &rest, &active_pos);
+      
+      if (POINT_LEN(active_pos) == RANGE_LEN(csfx)) {
+        /* yes: fast forward rule 3 for all other j's and increment i 
+         * we will restart at this same suffix (all other longer suffixes - 
+         * rule 1 - are already inserted because of the unbounded ranges trick),
+         * but it will be one character longer; thus the active_pos invariant
+         * is already taken care of */
         break;
       }
               
       /* rule 2: {j, i} not in the tree; do the split and add it */
-      active_node = splitatpoint(&headsp);
-    
-      int head_end = j + RANGE_LEN(active_node->node_val);
+      active_pos.parent = splitatpoint(&active_pos);
       range_t ss = {j, INT_MAX};
-      newchild(active_node, &ss);
+      newchild(active_pos.parent, &ss);
       
       /* active node is the point of the last insertion */
-      if (active_node->suffix_link == NULL) {
+      if (active_pos.parent->suffix_link == NULL) {
         splitpoint_t prefspl;
       
-        /* set the suffix link searching for {j, head_end}. use the suffix
+        /* set the suffix link searching for {j+1, i-1}. use the suffix
          * link of the parent to speed it up */
-        range_t prefix_nexthead = {j+1, head_end};
-        treenode_t *pfx_nexth_parent = active_node->parent->suffix_link;
+        range_t prefix_nexthead = {j+1, i-1};
+        treenode_t *pfx_nexth_parent = active_pos.parent->parent->suffix_link;
         range_t r = pfx_nexth_parent->node_val;
         prefix_nexthead.start += RANGE_LEN(r);
         prefspl = fastscan(str, &prefix_nexthead, pfx_nexth_parent);
       
         treenode_t *linkt = splitatpoint(&prefspl);
-        active_node->suffix_link = linkt;
+        active_pos.parent->suffix_link = linkt;
       }
-    
-      active_node = active_node->suffix_link;
+        
+      active_pos.parent = active_pos.parent->suffix_link;
+      active_pos.child = NULL;
+      active_pos.arcpos = 0;
     }
   }
 
